5f9bafcf7b03a272a4be3025ac7d9745d1db151f
Johan Almanzar <almanzar.j@northeastern.edu>
2025-01-11 06:22:57 -0500

    Created the PAWS button!

---
+++ make-my-bulletin/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+/**
+ * the top section of the website, which includes:
+ * the title 
+ * the color
+ */
+export default function Header() {
+    return (
+        <h1>MakeMy Bulletin</h1>
+    )
+}
