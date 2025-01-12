6ab4a893380de40cafb262cf39bfbec924b855d5
Johan Almanzar <almanzar.j@northeastern.edu>
2025-01-12 04:03:36 -0500

    Added the Button component! Need to specify the different Button types.

--- make-my-bulletin/src/App.jsx
+++ make-my-bulletin/src/App.jsx
@@ -3,11 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header.jsx'
+import Button from "./components/Button.jsx"
 
 function App() {
   return (
+  <>
     <Header />
-    
+    <div className="PAWS-button">
+      <Button professional="Professional Bulletin Board!" />
+      <Button professional="Professional Bulletin Board!" />
+      <Button professional="Professional Bulletin Board!" />
+      <Button professional="Professional Bulletin Board!" />
+    </div>
+  </>
   )
 }
 
---
+++ make-my-bulletin/src/components/Button.jsx
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param professional - a button which asks if this bulletin board is a professional one!
+ * @param affinity - a button which asks if this bulletin board is one relating to affinity
+ * @param wellbeing - a button which asks if a bulletin board is one relating to wellbeing 
+ * @param social - a button which asks if a bulletin board is one relating to social action
+ * 
+ */
+export default function Button({professional, affinity, wellbeing, social}) {
+    if (professional !== undefined) {
+        return (
+                <button>
+                    {professional}
+                </button>
+        )
+    }
+}
